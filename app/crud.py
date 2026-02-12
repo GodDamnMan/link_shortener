42b3b53a80376ea8d520fc32243754ad5d37c0b3
Marat Shariev <shariev.marat@mail.ru>
2026-02-12 15:35:29 +0300

    feat: add fuctionallity to get_original_url

--- app/crud.py
+++ app/crud.py
@@ -1,17 +1,19 @@
+import sqlite3
 
-
+from .database import get_connection
 
 def get_original_url(shorten_url:str) -> str | None:
     """
     Method that searches the original url in database
     Can return original url if found, or None if shorten_url
     is not presented in database
-
-    :param shorten_url: string of shorten url
-    :type shorten_url: str
-    :return: original url | None
-    :rtype: str | None
     """
-    
+    with get_connection() as conn:
+        row = conn.execute(
+            "SELECT original_url FROM urls WHERE shorten_url = ?", (shorten_url,)
+        ).fetchone()
+            
+        if row:
+            return row["original_url"]
 
-    return None
+        return None
